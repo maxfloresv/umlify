067a91d95941999955be830c678bf452afaf0d24
maxfloresv <maximo.flores@ug.uchile.cl>
2024-08-16 11:46:15 -0400

    ✨ Adds panel visualization

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import { ReactFlow } from '@xyflow/react';
+import { ReactFlow, Background, Controls, Panel } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-import './App.css';
+//import './App.css';
+
+const nodes = [
+  {
+    id: '1', // required
+    position: { x: 0, y: 0 }, // required
+    data: { label: 'Hello' }, // required
+  },
+];
 
 function App() {
   return (
-    <p>Hola mundo!</p>
+    <div style={{ height: 700 }}>
+      <ReactFlow nodes={nodes}>
+        <Panel
+          style={{ backgroundColor: "white" }}
+          position="top-right"
+        >
+          Panel interactivo
+        </Panel>
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
   );
 }
 
