5d016c5cbba21e785f8584cbef6fdda3db30ff95
maxfloresv <maximo.flores@ug.uchile.cl>
2024-08-19 11:49:54 -0400

    ✨ Adds button node handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import { ReactFlow, Background, Controls, Panel } from '@xyflow/react';
+import { useState } from 'react';
+import {
+  ReactFlow,
+  Background,
+  Controls,
+  Panel,
+  Edge,
+  Node
+} from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-//import './App.css';
 
-const nodes = [
-  {
-    id: '1', // required
-    position: { x: 0, y: 0 }, // required
-    data: { label: 'Hello' }, // required
-  },
-];
+import Button from '@mui/material/Button';
+
+const INITIAL_NODES: Node[] = [];
+const INITIAL_EDGES: Edge[] = [];
 
 function App() {
+  const [nodes, setNodes] = useState<Node[]>(INITIAL_NODES);
+  const [edges, setEdges] = useState<Edge[]>(INITIAL_EDGES);
+
+  function handleAddNode() {
+    const newNode: Node[] = [
+      {
+        id: String(nodes.length),
+        position: { x: 0, y: 0 },
+        data: { label: `Nodo ${nodes.length}` }
+      }
+    ];
+
+    setNodes([...nodes, ...newNode]);
+  }
+
   return (
     <div style={{ height: 700 }}>
-      <ReactFlow nodes={nodes}>
+      <ReactFlow nodes={nodes} edges={edges}>
         <Panel
           style={{ backgroundColor: "white" }}
           position="top-right"
         >
-          Panel interactivo
+          <Button variant="outlined" onClick={handleAddNode}>Añadir nodo</Button>
         </Panel>
         <Background />
         <Controls />
