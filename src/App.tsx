19536202a1a2878d5adc37d2d73ff0ac36cc5f4f
maxfloresv <maximo.flores@ug.uchile.cl>
2024-08-20 10:33:11 -0400

    ✨ Add node features

--- src/App.tsx
+++ src/App.tsx
@@ -19,11 +19,29 @@ function App() {
   const [edges, setEdges] = useState<Edge[]>(INITIAL_EDGES);
 
   function handleAddNode() {
+    const id = nodes.length;
+    // Minimum separation between the nodes and the canvas' borders.
+    const indent = 20;
+
+    // Separation between nodes in x-axis and y-axis.
+    const x_sep = 300;
+    const y_sep = 100;
+
+    // At which width we want to broke the nodes to change separation in y-axis.
+    const canvas_broke_width = 1250;
+
+    // Fixed position in (x, y) for this node.
+    const x_fixed = indent + x_sep * id;
+    const y_fixed = Math.floor(x_fixed / canvas_broke_width) * y_sep + indent;
+
+    // TODO: Search for node width
+    const _ = Math.floor(canvas_broke_width / x_fixed)
+
     const newNode: Node[] = [
       {
-        id: String(nodes.length),
-        position: { x: 0, y: 0 },
-        data: { label: `Nodo ${nodes.length}` }
+        id: String(id),
+        position: { x: x_fixed, y: y_fixed },
+        data: { label: `Nodo ${id}` }
       }
     ];
 
