b188bbf9cb188384f0d2404d6d0c62478616e62c
maxfloresv <maximo.flores@ug.uchile.cl>
2024-08-26 19:37:00 -0400

    ✨ Adds abstract parameters

--- src/model/UMLAbstractClass.ts
+++ src/model/UMLAbstractClass.ts
@@ -4,6 +4,9 @@ import UMLNode from "./UMLNode";
 abstract class UMLAbstractClass implements UMLNode {
   abstract id: number;
   abstract name: string;
+  abstract node: Node;
+  abstract methods: Object;
+  abstract attributes: Object;
   protected extends: UMLNode[] = [];
   x: number;
   y: number;
@@ -12,20 +15,21 @@ abstract class UMLAbstractClass implements UMLNode {
     this.x = x;
     this.y = y;
   }
+
   addExtends: (c: UMLNode) => void = (c) => {
     this.extends.push(c);
   };
+
   removeExtends: (c: UMLNode) => void = (c) => {
     this.extends = this.extends.filter((e) => e.id !== c.id);
   };
 
-  getNode: () => Node = () => {
-    return {
-      id: String(this.id),
-      position: { x: this.x, y: this.y },
-      data: { label: `Nodo ${this.id}` },
-    };
-  };
+  getNode: () => Node = () => this.node;
+  updateNode: (newNode: Node) => void = (newNode) => {
+    this.x = newNode.position.x;
+    this.y = newNode.position.y;
+    this.node = newNode; 
+  }
 }
 
 export default UMLAbstractClass;
