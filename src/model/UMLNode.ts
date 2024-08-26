cbc8dc4ac2677a54c2788396f4a66944bd7ea54b
maxfloresv <maximo.flores@ug.uchile.cl>
2024-08-26 19:37:12 -0400

    ✨ Adds new types to the interface

--- src/model/UMLNode.ts
+++ src/model/UMLNode.ts
@@ -3,7 +3,11 @@ import { Node } from "@xyflow/react";
 export default interface UMLNode {
   id: number;
   name: string;
+  methods: Object;
+  attributes: Object;
   addExtends: (c: UMLNode) => void;
   removeExtends: (c: UMLNode) => void;
+  node: Node;
   getNode: () => Node;
+  updateNode: (n: Node) => void;
 }
