e8ec0f22af4bc6ab0e1ccde1598f2b4393f3e876
maxfloresv <maximo.flores@ug.uchile.cl>
2024-08-26 19:36:45 -0400

    ✨ Adds methods, attributes, and node data

--- src/model/ConcreteClass.ts
+++ src/model/ConcreteClass.ts
@@ -4,11 +4,27 @@ import UMLAbstractClass from "./UMLAbstractClass";
 class ConcreteClass extends UMLAbstractClass {
   id: number;
   name: string;
+  methods: Object;
+  attributes: Object;
+  node: Node;
 
-  constructor(id: number, x: number, y: number) {
+  constructor(id: number, name: string, methods: Object, attributes: Object, x: number, y: number) {
     super(x, y);
-    this.name = "Foo";
+    this.name = name;
     this.id = id;
+    this.methods = methods;
+    this.attributes = attributes;
+    this.node = {
+      id: String(this.id),
+      type: 'concreteClass',
+      position: { x: this.x, y: this.y },
+      data: { 
+        name: this.name, 
+        methods: this.methods, 
+        attributes: this.attributes,
+        styleClass: 'concrete-paragraph',
+      },
+    };
   }
 }
 
