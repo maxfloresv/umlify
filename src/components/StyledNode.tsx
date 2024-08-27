6ffe5b7cabfd227a657917adb4f194fdd703fa11
matia storo <mtoro.cl@gmail.com>
2024-08-27 12:08:37 -0400

    name change

---
+++ src/components/StyledNode.tsx
@@ -0,0 +1,71 @@
+import { Handle, Node, NodeProps, Position } from "@xyflow/react";
+import "./css/paragraph.css";
+import { FieldType, MethodType, Visibility } from "../model/UMLNode";
+import { ComponentType } from "react";
+import type { AbstractClassDataType } from "../model/AbstractClass";
+
+//TODO: fix
+type Foo = Node<AbstractClassDataType | any, "Node data">;
+
+function StyledNode({ data }: NodeProps<Foo>) {
+  const drawVisibility = (v: Visibility | null) => {
+    switch (v) {
+      case "public":
+        return "+";
+      case "protected":
+        return "#";
+      case "private":
+        return "-";
+      default:
+        return "";
+    }
+  };
+  //const data: any = node.data; //TODO: address this!
+  return (
+    <>
+      <Handle type="target" position={Position.Top} />
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          backgroundColor: "white",
+        }}
+      >
+        <div style={{ border: "1px solid black", width: "100%" }}>
+          {data.additionalText && data.additionalText.length > 0 && (
+            <p style={{ textAlign: "center" }}>{data.additionalText}</p>
+          )}
+          <p className={data.styleClass}>{data.name}</p>
+        </div>
+        <div style={{ border: "1px solid black", width: "100%" }}>
+          {data.fields.map((field: FieldType) => {
+            return (
+              <p key={field.name}>
+                {drawVisibility(field.visibility)} {field.name}: {field.type}
+              </p>
+            );
+          })}
+        </div>
+        <div style={{ border: "1px solid black", width: "100%" }}>
+          {data.methods.map((method: MethodType) => {
+            return (
+              <p key={method.name}>
+                {drawVisibility(method.visibility)} {method.name}
+                {"("}
+                {method.domType.map((t) => t).join(", ")}
+                {")"}
+                {method.codType ? ":" + method.codType : ""}
+              </p>
+            );
+          })}
+        </div>
+      </div>
+
+      <Handle type="source" position={Position.Bottom} id="a" />
+    </>
+  );
+}
+
+export default StyledNode;
