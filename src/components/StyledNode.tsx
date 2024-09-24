ab45b07da103f988e86c1623084540d39503265f
maxfloresv <maximo.flores@ug.uchile.cl>
2024-09-23 22:22:02 -0300

    ✨ Introduces context to styled node

--- src/components/StyledNode.tsx
+++ src/components/StyledNode.tsx
@@ -1,7 +1,7 @@
 import { Handle, Node, NodeProps, Position } from "@xyflow/react";
 import "./css/paragraph.css";
 import "./css/containers.css";
-import { FieldType, MethodType, Visibility } from "../model/UMLNode";
+import UMLNode, { FieldType, MethodType, Visibility } from "../model/UMLNode";
 import type { CustomNodeData } from "../model/UMLNode";
 
 import EditIcon from '@mui/icons-material/Edit';
@@ -10,145 +10,164 @@ import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { IconButton, TextField, Accordion, AccordionSummary, AccordionDetails, Typography, Button } from "@mui/material";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useGlobalContext from "../hooks/useGlobalContext";
 
 type ClassType = "trait" | "concreteClass" | "abstractClass";
 type CustomNode = Node<CustomNodeData, ClassType>;
 
-function StyledNode({ data }: NodeProps<CustomNode>) {
-  const drawVisibility = (v: Visibility | null) => {
-    switch (v) {
-      case "public":
-        return "+";
-      case "protected":
-        return "#";
-      case "private":
-        return "-";
-      default:
-        return "?";
-    }
-  };
-
+// TODO: fix this with the correct type
+function StyledNode(ctx: ReturnType<typeof useGlobalContext>) {
   // TODO: This can't be moved to ctx?
   const [editMode, setEditMode] = useState<boolean>(false);
 
-  return (
-    <>
-      <Handle type="target" position={Position.Top} />
+  // We need this in order to re-render the component when the fields are updated.
+  const [currentFields, setCurrentFields] = useState<FieldType[]>([]);
 
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-          backgroundColor: "white",
-          minWidth: "200px",
-        }}
-      >
-        <div style={{ border: "1px solid black", width: "100%" }}>
-          {data.additionalText && data.additionalText.length > 0 && (
-            <p style={{ textAlign: "center" }}>{data.additionalText}</p>
-          )}
-          <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+  // TODO: how can we handle the component update when the fields are updated?
+  //useEffect(() => { }, [currentFields]);
+
+  return ({ data }: NodeProps<CustomNode>) => {
+    setCurrentFields(data.fields);
+
+    const drawVisibility = (v: Visibility | null) => {
+      switch (v) {
+        case "public":
+          return "+";
+        case "protected":
+          return "#";
+        case "private":
+          return "-";
+        default:
+          return "?";
+      }
+    };
+
+    return (
+      <>
+        <Handle type="target" position={Position.Top} />
+
+        <div className="box-container">
+          <div className="title-container">
+            {data.additionalText && data.additionalText.length > 0 && (
+              <p className="additional-text-paragraph">{data.additionalText}</p>
+            )}
             {!editMode ?
               <p className={data.styleClass}>{data.name}</p> :
               <TextField
                 id="standard-basic"
-                label="Nombre"
+                label="Name"
                 variant="standard"
                 defaultValue={data.name}
-              //onChange={(e) => data.setName(e.target.value)}
+                className="nodrag"
+                onChange={(e) => {
+                  const [retrievedNode] = ctx.nodes.filter((n: UMLNode) => n.id === data.id);
+                  retrievedNode.updateName(e.target.value);
+                }}
               />
             }
-            <IconButton onClick={() => setEditMode(true)}>
-              <EditIcon />
-            </IconButton>
-            {editMode && <IconButton onClick={() => setEditMode(false)}>
-              <SaveIcon />
-            </IconButton>}
+            <div style={{ position: "absolute", top: 0, right: 0 }}>
+              {editMode ? <IconButton onClick={() => setEditMode(false)}>
+                <SaveIcon />
+              </IconButton> :
+                <IconButton onClick={() => setEditMode(true)}>
+                  <EditIcon />
+                </IconButton>}
+            </div>
+          </div>
+
+          <div className="field-container">
+            {!editMode ?
+              currentFields.map((field: FieldType, id: number) => {
+                return (
+                  <p key={id}>
+                    {drawVisibility(field.visibility)} {field.name}: {field.type}
+                  </p>
+                );
+              }) :
+              <>
+                <Button onClick={() => {
+                  const [retrievedNode] = ctx.nodes.filter((n: UMLNode) => n.id === data.id);
+                  const newField: FieldType = {
+                    name: "newField",
+                    type: "String",
+                    visibility: "public"
+                  };
+
+                  retrievedNode.addField(newField);
+                  setCurrentFields(data.fields);
+                }} variant="text">Add field</Button>
+
+                {currentFields.map((field: FieldType, i: number) => {
+                  return (
+                    <Accordion>
+                      <AccordionSummary
+                        expandIcon={<ArrowDownwardIcon />}
+                        aria-controls={`panel${i}-content`}
+                        id={`panel${i}-header`}
+                      >
+                        <Typography>{drawVisibility(field.visibility)} {field.name}</Typography>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        <Typography>
+                          {drawVisibility(field.visibility)} {field.name}: {field.type}
+                        </Typography>
+                      </AccordionDetails>
+                    </Accordion>
+                  )
+                })}
+              </>
+            }
+          </div>
+
+          <div className="method-container">
+            {!editMode ?
+              data.methods.map((method: MethodType, id: number) => {
+                return (
+                  <p key={id}>
+                    {drawVisibility(method.visibility)} {method.name}
+                    {"("}
+                    {method.domType.map((t) => t).join(", ")}
+                    {")"}
+                    {method.codType ? ": " + method.codType : ""}
+                  </p>
+                );
+              }) :
+              <>
+                <Button variant="text">Add method</Button>
+                {data.methods.map((method: MethodType, id: number) => {
+                  return (
+                    <Accordion>
+                      <AccordionSummary
+                        expandIcon={<ArrowDownwardIcon />}
+                        aria-controls={`panel${id}-content`}
+                        id={`panel${id}-header`}
+                      >
+                        <Typography>{drawVisibility(method.visibility)} {method.name}</Typography>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        <Typography>
+                          <p key={id}>
+                            {drawVisibility(method.visibility)} {method.name}
+                            {"("}
+                            {method.domType.map((t) => t).join(", ")}
+                            {")"}
+                            {method.codType ? ": " + method.codType : ""}
+                          </p>
+                        </Typography>
+                      </AccordionDetails>
+                    </Accordion>
+                  )
+                })}
+              </>
+            }
           </div>
         </div>
 
-        <div className="field-container">
-          {!editMode ?
-            data.fields.map((field: FieldType) => {
-              return (
-                <p key={field.name}>
-                  {drawVisibility(field.visibility)} {field.name}: {field.type}
-                </p>
-              );
-            }) :
-            <>
-              <Button variant="text">Añadir campo</Button>
-              {data.fields.map((field: FieldType, i: number) => {
-                return (
-                  <Accordion>
-                    <AccordionSummary
-                      expandIcon={<ArrowDownwardIcon />}
-                      aria-controls={`panel${i}-content`}
-                      id={`panel${i}-header`}
-                    >
-                      <Typography>{drawVisibility(field.visibility)} {field.name}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <Typography>
-                        {drawVisibility(field.visibility)} {field.name}: {field.type}
-                      </Typography>
-                    </AccordionDetails>
-                  </Accordion>
-                )
-              })}
-            </>
-          }
-        </div>
-
-        <div className="method-container">
-          {!editMode ?
-            data.methods.map((method: MethodType) => {
-              return (
-                <p key={method.name}>
-                  {drawVisibility(method.visibility)} {method.name}
-                  {"("}
-                  {method.domType.map((t) => t).join(", ")}
-                  {")"}
-                  {method.codType ? ": " + method.codType : ""}
-                </p>
-              );
-            }) :
-            <>
-              <Button variant="text">Añadir método</Button>
-              {data.methods.map((method: MethodType, i: number) => {
-                return (
-                  <Accordion>
-                    <AccordionSummary
-                      expandIcon={<ArrowDownwardIcon />}
-                      aria-controls={`panel${i}-content`}
-                      id={`panel${i}-header`}
-                    >
-                      <Typography>{drawVisibility(method.visibility)} {method.name}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <Typography>
-                        <p key={method.name}>
-                          {drawVisibility(method.visibility)} {method.name}
-                          {"("}
-                          {method.domType.map((t) => t).join(", ")}
-                          {")"}
-                          {method.codType ? ": " + method.codType : ""}
-                        </p>
-                      </Typography>
-                    </AccordionDetails>
-                  </Accordion>
-                )
-              })}
-            </>
-          }
-        </div>
-      </div>
-
-      <Handle type="source" position={Position.Bottom} id="a" />
-    </>
-  );
+        <Handle type="source" position={Position.Bottom} id="a" />
+      </>
+    );
+  };
 }
 
 export default StyledNode;
