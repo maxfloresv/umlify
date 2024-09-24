48d304cd3498a3a571c0ad78ea4daec351052997
maxfloresv <maximo.flores@ug.uchile.cl>
2024-09-24 10:04:12 -0300

    ✨ Implements new buttons and forms

--- src/components/StyledNode.tsx
+++ src/components/StyledNode.tsx
@@ -6,12 +6,15 @@ import type { CustomNodeData } from "../model/UMLNode";
 
 import EditIcon from '@mui/icons-material/Edit';
 import SaveIcon from '@mui/icons-material/Save';
+import CloseIcon from '@mui/icons-material/Close';
+import DeleteIcon from '@mui/icons-material/Delete';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import { IconButton, TextField, Accordion, AccordionSummary, AccordionDetails, Typography, Button } from "@mui/material";
+import { IconButton, TextField, Accordion, AccordionSummary, AccordionDetails, Typography, Button, FormControl, InputLabel, Select, MenuItem, Box } from "@mui/material";
 
 import { useEffect, useState } from "react";
 import useGlobalContext from "../hooks/useGlobalContext";
+import { width } from "@mui/system";
 
 type ClassType = "trait" | "concreteClass" | "abstractClass";
 type CustomNode = Node<CustomNodeData, ClassType>;
@@ -54,8 +57,9 @@ const StyledNode = (props: StyledNodeProps) => {
           {!editMode ?
             <p className={data.styleClass}>{data.name}</p> :
             <TextField
-              id="standard-basic"
-              label="Name"
+              id="class-text-name"
+              sx={{ width: "100%" }}
+              label="Class Name"
               variant="standard"
               defaultValue={data.name}
               className="nodrag"
@@ -72,6 +76,12 @@ const StyledNode = (props: StyledNodeProps) => {
               <IconButton onClick={() => setEditMode(true)}>
                 <EditIcon />
               </IconButton>}
+          </div>
+
+          <div style={{ position: "absolute", top: 0, left: 0 }}>
+            {editMode && <IconButton onClick={() => null}>
+              <CloseIcon />
+            </IconButton>}
           </div>
         </div>
 
@@ -100,17 +110,92 @@ const StyledNode = (props: StyledNodeProps) => {
               {currentFields.map((field: FieldType, i: number) => {
                 return (
                   <Accordion>
-                    <AccordionSummary
-                      expandIcon={<ArrowDownwardIcon />}
-                      aria-controls={`panel${i}-content`}
-                      id={`panel${i}-header`}
-                    >
-                      <Typography>{drawVisibility(field.visibility)} {field.name}</Typography>
-                    </AccordionSummary>
+                    <Box sx={{ display: "flex", minWidth: "100%" }}>
+                      <div style={{ width: "100%" }}>
+                        <AccordionSummary
+                          expandIcon={<ArrowDownwardIcon />}
+                          aria-controls={`panel${i}-content`}
+                          id={`panel${i}-header`}
+                        >
+                          <Typography>{drawVisibility(field.visibility)} {field.name}: {field.type}</Typography>
+                        </AccordionSummary>
+                      </div>
+
+                      <div style={{ width: "fit-content", alignContent: "center" }}>
+                        <IconButton>
+                          <DeleteIcon onClick={() => {
+                            const [retrievedNode] = nodeList.filter((n: UMLNode) => n.id === data.id);
+                            retrievedNode.removeField(field);
+                            setCurrentFields(data.fields);
+                          }} />
+                        </IconButton>
+                      </div>
+                    </Box>
                     <AccordionDetails>
-                      <Typography>
-                        {drawVisibility(field.visibility)} {field.name}: {field.type}
-                      </Typography>
+                      <div className="field-text-edit-container">
+                        <TextField
+                          id={`field-${i}-name`}
+                          label="Field Name"
+                          variant="standard"
+                          defaultValue={field.name}
+                          className="nodrag"
+                          onChange={(e) => {
+                            const [retrievedNode] = nodeList.filter((n: UMLNode) => n.id === data.id);
+                            const fieldToUpdate = data.fields.find((f) => f.name === field.name);
+
+                            if (!fieldToUpdate) {
+                              return;
+                            }
+
+                            retrievedNode.updateField(fieldToUpdate, { ...fieldToUpdate, name: e.target.value });
+                          }}
+                        />
+
+                        <TextField
+                          id={`field-${i}-type`}
+                          label="Field Type"
+                          variant="standard"
+                          defaultValue={field.type}
+                          className="nodrag"
+                          onChange={(e) => {
+                            const [retrievedNode] = nodeList.filter((n: UMLNode) => n.id === data.id);
+                            const fieldToUpdate = data.fields.find((f) => f.name === field.name);
+
+                            if (!fieldToUpdate) {
+                              return;
+                            }
+
+                            retrievedNode.updateField(fieldToUpdate, { ...fieldToUpdate, type: e.target.value });
+                          }}
+                        />
+                      </div>
+
+                      <FormControl fullWidth>
+                        <InputLabel id={`field-${i}-visibility`}>Visibility</InputLabel>
+                        <Select
+                          labelId={`field-${i}-visibility`}
+                          id={`field-${i}-visibility-select`}
+                          sx={{ overflow: "visible" }}
+                          value={field.visibility}
+                          label="Visibility"
+                          onChange={(e) => {
+                            const [retrievedNode] = nodeList.filter((n: UMLNode) => n.id === data.id);
+                            const fieldToUpdate = data.fields.find((f) => f.name === field.name);
+
+                            if (!fieldToUpdate) {
+                              return;
+                            }
+
+                            console.log(e.target.value);
+
+                            retrievedNode.updateField(fieldToUpdate, { ...fieldToUpdate, visibility: field.visibility });
+                          }}
+                        >
+                          <MenuItem value={"public"}>Public</MenuItem>
+                          <MenuItem value={"protected"}>Protected</MenuItem>
+                          <MenuItem value={"private"}>Private</MenuItem>
+                        </Select>
+                      </FormControl>
                     </AccordionDetails>
                   </Accordion>
                 )
