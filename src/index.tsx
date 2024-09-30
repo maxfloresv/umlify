a66c07bd4f824066070265693551b5ba62824ef7
maxfloresv <maximo.flores@ug.uchile.cl>
2024-09-29 21:14:26 -0300

    ✨ Creates a theme provider to force nodrag

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+// nodrag prevents rare bugs when editing a node.
+const theme = createTheme({
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        className: 'nodrag',
+      },
+    },
+    MuiTextField: {
+      defaultProps: {
+        className: 'nodrag',
+      },
+    },
+    MuiSelect: {
+      defaultProps: {
+        className: 'nodrag',
+      },
+    }
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
