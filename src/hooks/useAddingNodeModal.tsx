e2a2c4c400271212d0a8e8f1d20f1bf1b19c98c4
maxfloresv <maximo.flores@ug.uchile.cl>
2024-09-29 22:49:56 -0300

    ⚰️ Removes unused code

--- src/hooks/useAddingNodeModal.tsx
+++ src/hooks/useAddingNodeModal.tsx
@@ -1,10 +1,6 @@
 import { useState } from "react";
 import UMLNode from "../model/UMLNode";
 
-type A = {
-
-};
-
 const useAddingNodeModal = () => {
   /** Whether the user has opened the node modal (i.e. clicked an option when right-clicked). */
   const [editMode, setEditMode] = useState<boolean>(false);
